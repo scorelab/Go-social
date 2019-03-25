cce2cd3a4a43c1c520f47ea245c8bb1aecd12ea6
lahirutech <rmllbrathnayaka@gmail.com>
2019-03-25 23:36:12 +0530

    Adding the Docs folder

--- Docs/src/index.js
+++ Docs/src/index.js
@@ -1,8 +1,12 @@
+import fierbase from 'firebase';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
