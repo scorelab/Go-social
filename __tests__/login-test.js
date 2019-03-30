a913698a1334cf1574663114588b2a83c1cc187f
shehand <sonodewcomplex@gmail.com>
2019-03-30 17:50:49 +0530

    rewrite the login test

--- __tests__/login-test.js
+++ __tests__/login-test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import LoginScreen from '../app/screens/LoginScreen/loginScreen';
+import loginScreen from '../app/screens/LoginScreen/loginScreen';
 
 import renderer from 'react-test-renderer';
 
-test('renders correctly',() => {
+it('renders correctly',() => {
     const tree = renderer.create('<LoginScreen/>').toJSON();
     expect(tree).toMatchSnapshot();
 })
