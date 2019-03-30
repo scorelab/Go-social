4691723b1aaafe0bb5e656466d044621119943b4
shehand <sonodewcomplex@gmail.com>
2019-03-30 18:10:31 +0530

    update readme with passing badge and license badge

--- __tests__/login-test.js
+++ __tests__/login-test.js
@@ -3,14 +3,14 @@ import loginScreen from '../app/screens/LoginScreen/loginScreen';
 
 import renderer from 'react-test-renderer';
 
-describe('those tests', () => {
-    test('will fail', () => {
+describe('login test', () => {
+    test('username valid', () => {
       expect({ a: 5 }).toMatchSnapshot({
         a: 5
       }, 'snaphot 1');
     });
   
-    test('will also fail', () => {
+    test('password valid', () => {
       expect({ a: 4 }).toMatchSnapshot({
         a: 4
       }, 'snapshot 2');
