0d796dd8a27a226e025a359e27fb5182d77e7f46
Abhi Kulshrestha <40908684+AbhiKul10@users.noreply.github.com>
2021-05-28 21:09:04 +0530

    Updated Code Structure

--- app/components/ForumBox/ForumBox.js
+++ app/components/ForumBox/ForumBox.js
@@ -3,23 +3,18 @@ import {View, Text, TextInput, StyleSheet, TouchableOpacity} from 'react-native'
 import styles from './styles';
 
 export default class ForumBox extends React.Component {
-    render() {
-    return (
-        <View style={{marginTop: 15}}>
-                
-                <View style = {styles.QuestionStyle}>
-                    <View style={styles.nameHeadlineStyle}>
-                        <Text style={{marginLeft: 10}}>{this.props.Question}</Text>
-                    </View>
-                    <View style={{backgroundColor:'white', borderRadius:10}}>
-                        <TextInput style={{padding: 10, fontSize: 13, width: "90%", height: 80 }} multiline  placeholder = "Ask your query, by addressing your problem clearly!" />
-                        
-                    </View>
-                    
-
-                </View>
-
-            </View>
-        );
-    }
+	render() {
+		return (
+			<View style={{marginTop: 15}}>
+				<View style = {styles.QuestionStyle}>
+					<View style={styles.nameHeadlineStyle}>
+						<Text style={{marginLeft: 10}}>{this.props.Question}</Text>
+					</View>
+					<View style={{backgroundColor:'white', borderRadius:10}}>
+						<TextInput style={{padding: 10, fontSize: 13, width: "90%", height: 80 }} multiline  placeholder = "Ask your query, by addressing your problem clearly!" />
+					</View>
+				</View>
+			</View>
+		);
+	}
 }
--- app/components/HeaderNavigationBar/styles.js
+++ app/components/HeaderNavigationBar/styles.js
@@ -5,30 +5,30 @@ let deviceWidth = Dimensions.get("window").width;
 let deviceHeight = Dimensions.get("window").height;
 
 export default StyleSheet.create({
-    navigationBar: {
-        backgroundColor: COLOR_PRIMARY,
-        height: 55,
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        width:"100%"
-    },
-    leftIconContainer: {
-        marginLeft: 10,
-        marginTop: 5,
-        borderRadius: 5
-    },
-    rightIconContainer: {
-        marginLeft: 10,
-        marginTop: 5,
-        borderRadius: 5
-    },
-    titleArea:{
-        width:deviceWidth*0.5,
-        alignItems:"center"
-    },
-    titleFont:{
-        fontSize:18,
-        color:"white"
-    }
+	navigationBar: {
+		backgroundColor: COLOR_PRIMARY,
+		height: 55,
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+		width:"100%"
+	},
+	leftIconContainer: {
+		marginLeft: 10,
+		marginTop: 5,
+		borderRadius: 5
+	},
+	rightIconContainer: {
+		marginLeft: 10,
+		marginTop: 5,
+		borderRadius: 5
+	},
+	titleArea:{
+		width:deviceWidth*0.5,
+		alignItems:"center"
+	},
+	titleFont:{
+		fontSize:18,
+		color:"white"
+	}
 });
