a84eb999bc33467d3278148e30f0d23351ead489
kanha <kanha.agr11@gmail.com>
2022-03-21 12:46:17 +0530

    styles typo in formscreen.js

--- app/screens/ForumScreen/forumScreen.js
+++ app/screens/ForumScreen/forumScreen.js
@@ -2,14 +2,14 @@ import React, { Component } from "react";
 import { ScrollView, Text, View, TouchableOpacity, Image } from "react-native";
 import HeaderBar from '../../components/HeaderBar/HeaderBar';
 import ForumBox from '../../components/ForumBox/ForumBox';
-import styles from './style';
+import styles from './styles';
 
 
-export default class ForumScreen extends Component {    
+export default class ForumScreen extends Component {
     render() {
         return (
             <View>
-                <HeaderBar image={require("../../images/Ask-SCORE-Logo.png")} title={"Home"} />                          
+                <HeaderBar image={require("../../images/Ask-SCORE-Logo.png")} title={"Home"} />
 
                 <ScrollView style={styles.scrollView} contentContainerStyle={styles.scrollViewContent} showsVerticalScrollIndicator={false}>
                     <ForumBox Question="Enter your Question here" />
