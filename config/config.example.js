1dd84b840716430d1e42e29b0f85cd533366d53b
yash621 <yg17381@gmail.com>
2022-03-24 00:39:01 +0530

    recerted linting

--- config/config.example.js
+++ config/config.example.js
@@ -12,10 +12,10 @@ var config = {
 };
  !firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
 
- var MAP_API_KEY = "";
+  var MAP_API_KEY = "";
 
- export const f = firebase;
- export const database = firebase.database();
- export const auth = firebase.auth();
- export const storage = firebase.storage();
- export const MAP_API = MAP_API_KEY;
+  export const f = firebase;
+  export const database = firebase.database();
+  export const auth = firebase.auth();
+  export const storage = firebase.storage();
+  export const MAP_API = MAP_API_KEY;
