c9c9087e27e79f8fe3bc5c6cec8da8a0819fc7ab
kanha <kanha.agr11@gmail.com>
2022-03-24 21:57:07 +0530

    fix for Throw an error "Object is not iterable" at the app start #128

--- config/config.example.js
+++ config/config.example.js
@@ -7,13 +7,17 @@ var config = {
   projectId: "",
   storageBucket: "",
   messagingSenderId: ""
-  };
+};
+try {
   firebase.initializeApp(config);
+} catch (error) {
+  console.log(error)
+}
 
-  var MAP_API_KEY = "";
+var MAP_API_KEY = "";
 
-  export const f = firebase;
-  export const database = firebase.database();
-  export const auth = firebase.auth();
-  export const storage = firebase.storage();
-  export const MAP_API = MAP_API_KEY;
+export const f = firebase;
+export const database = firebase.database();
+export const auth = firebase.auth();
+export const storage = firebase.storage();
+export const MAP_API = MAP_API_KEY;
